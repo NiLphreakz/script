0f999e075eb5a6514cf5abe1b29fc7ba52b1cb3b
NiL <134828606+NiLphreakz@users.noreply.github.com>
2023-08-04 18:27:53 +0800

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "# Script Auto Install SSH           #"
+echo -e "#-----------------------------------------------#"
+echo -e "# For Debian 10 64bit              #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Modified ~NiLphreakz~                              #"
+echo -e "#-----------------------------------------------#"
+echo -e "Thanks To"
+echo -e "---------"
+echo -e "Allah"
+echo -e "Bustami Arifin (Original script)"
+echo -e ""
+echo -e "#-----------------------------------------------#"
+echo -e "================================================="
---
+++ resvis.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+service dropbear restart
+service webmin restart
+service squid3 restart
+service ssh restart
+service stunnel4 restart
